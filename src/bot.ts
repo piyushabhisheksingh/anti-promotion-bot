be6a592a40d0b507d1c70d985e973d15a50ae6c6
Piyush Abhishek Singh <piyush.singh@gosite.com>
2025-03-08 16:03:05 +0530

    logger

--- src/bot.ts
+++ src/bot.ts
@@ -370,7 +370,7 @@ bot.filter(ctx => ctx.chat?.type != 'private').on(["chat_member", ":new_chat_mem
   }
 })
 
-bot.filter(ctx => ctx.chat?.type != 'private').hears(/[\s\S]*/, async (ctx) => {
+bot.filter(ctx => ctx.chat?.type != 'private').hears(/.*/, async (ctx) => {
   if (ctx.session.userList.exceptionList.includes(ctx.from?.id ?? 0)) {
     return
   }
